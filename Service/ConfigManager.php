8428863fd121ba5e86a03cc5da2e633566dd76ce
David Yang <daworld.ny@gmail.com>
2016-07-01 00:16:46 +0500

    Added missing defaults

--- Service/ConfigManager.php
+++ Service/ConfigManager.php
@@ -23,11 +23,11 @@ final class ConfigManager extends ConfigModuleService
     {
         $entity = new VirtualEntity();
 
-        $entity->setShowCaseCount($this->get('showcase_count'), VirtualEntity::FILTER_INT)
-               ->setSpecialSupport($this->get('special_support'), VirtualEntity::FILTER_BOOL)
-               ->setStokePriceEnabled($this->get('stoke_price_enabled'), VirtualEntity::FILTER_BOOL)
-               ->setDefaultCategoryPerPageCount($this->get('default_category_per_page_count'), VirtualEntity::FILTER_INT)
-               ->setCurrency($this->get('currency'), VirtualEntity::FILTER_TAGS)
+        $entity->setShowCaseCount($this->get('showcase_count', 3), VirtualEntity::FILTER_INT)
+               ->setSpecialSupport($this->get('special_support', true), VirtualEntity::FILTER_BOOL)
+               ->setStokePriceEnabled($this->get('stoke_price_enabled', false), VirtualEntity::FILTER_BOOL)
+               ->setDefaultCategoryPerPageCount($this->get('default_category_per_page_count', 5), VirtualEntity::FILTER_INT)
+               ->setCurrency($this->get('currency', '$'), VirtualEntity::FILTER_TAGS)
                ->setMaxRecentAmount($this->get('recent_max_amount', 3), VirtualEntity::FILTER_INT)
                ->setBasketPageId($this->get('basket_page_id', 0), VirtualEntity::FILTER_INT)
                ->setStokePerPageCount($this->get('stoke_per_page_count', 10), VirtualEntity::FILTER_INT);
@@ -38,12 +38,12 @@ final class ConfigManager extends ConfigModuleService
             'cookies' => 'Save data forever (In cookies)'
         ));
 
-        $entity->setCoverHeight($this->get('cover_height'), VirtualEntity::FILTER_FLOAT)
-               ->setCoverWidth($this->get('cover_width'), VirtualEntity::FILTER_FLOAT)
-               ->setThumbHeight($this->get('thumb_height'), VirtualEntity::FILTER_FLOAT)
-               ->setThumbWidth($this->get('thumb_width'), VirtualEntity::FILTER_FLOAT)
-               ->setCategoryCoverHeight($this->get('category_cover_height'), VirtualEntity::FILTER_FLOAT)
-               ->setCategoryCoverWidth($this->get('category_cover_width'), VirtualEntity::FILTER_FLOAT);
+        $entity->setCoverHeight($this->get('cover_height', 300), VirtualEntity::FILTER_FLOAT)
+               ->setCoverWidth($this->get('cover_width', 300), VirtualEntity::FILTER_FLOAT)
+               ->setThumbHeight($this->get('thumb_height', 300), VirtualEntity::FILTER_FLOAT)
+               ->setThumbWidth($this->get('thumb_width', 300), VirtualEntity::FILTER_FLOAT)
+               ->setCategoryCoverHeight($this->get('category_cover_height', 300), VirtualEntity::FILTER_FLOAT)
+               ->setCategoryCoverWidth($this->get('category_cover_width', 300), VirtualEntity::FILTER_FLOAT);
 
         return $entity;
     }
