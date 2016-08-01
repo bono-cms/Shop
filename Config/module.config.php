81f880648ca9d6f280cafe3b935009389ddac98a
David Yang <daworld.ny@gmail.com>
2016-08-01 22:29:30 +0500

    Removed extras from module configuration file

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,11 +1,9 @@
 <?php
 
 return array(
-    'module' => array(
-        'name' => 'Shop',
-        'route' => 'Shop:Admin:Browser@indexAction',
-        'icon' => 'fa fa-shopping-cart fa-5x',
-        'order' => 1,
-        'description' => 'Shop module allows you to manage e-commerce system on your site'
-    )
+ 'name' => 'Shop',
+ 'route' => 'Shop:Admin:Browser@indexAction',
+ 'icon' => 'fa fa-shopping-cart fa-5x',
+ 'order' => 1,
+ 'description' => 'Shop module allows you to manage e-commerce system on your site'
 );
