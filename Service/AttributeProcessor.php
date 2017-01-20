c488489d4c7ddcc44efbb69ff6e15e99e8863dc9
David Yang <daworld.ny@gmail.com>
2017-01-20 23:51:50 +0500

    Added AttributeProcessor tool

---
+++ Service/AttributeProcessor.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Shop\Service;
+
+/**
+ * The purpose of this tool is to convert a raw result-set
+ * that gets returned from SQL query in CategoryMapper::findAttributesById()
+ * into a nice-looking set that will be easy to read and iterate over in templates
+ */
+final class AttributeProcessor
+{
+    /**
+     * A collection of rows
+     * 
+     * @var array
+     */
+    private $rows = array();
+
+    const ARRAY_KEY_GROUP_ID = 'group_id';
+    const ARRAY_KEY_GROUP_NAME = 'group_name';
+    const ARRAY_KEY_ATTRIBUTES = 'attributes';
+    const ARRAY_KEY_VALUE_ID = 'value_id';
+    const ARRAY_KEY_VALUE_NAME = 'value_name';
+
+    /**
+     * State initialization
+     * 
+     * @param array $rows
+     * @return void
+     */
+    public function __construct(array $rows)
+    {
+        $this->rows = $rows;
+    }
+
+    /**
+     * Creates attribute group
+     * 
+     * @return array
+     */
+    public function process()
+    {
+        $output = array();
+
+        foreach ($this->rows as $row) {
+            foreach ($output as $inner) {
+                if ($inner[self::ARRAY_KEY_GROUP_ID] == $row[self::ARRAY_KEY_GROUP_ID]) {
+                    continue 2;
+                }
+            }
+
+            $output[] = array(
+                self::ARRAY_KEY_GROUP_ID => $row[self::ARRAY_KEY_GROUP_ID],
+                self::ARRAY_KEY_GROUP_NAME => $row[self::ARRAY_KEY_GROUP_NAME],
+                self::ARRAY_KEY_ATTRIBUTES => $this->findAttrsByGroupId($row[self::ARRAY_KEY_GROUP_ID])
+            );
+        }
+
+        return $output;
+    }
+
+    /**
+     * Create attribute values by associated group ID
+     * 
+     * @param string $groupId
+     * @return array
+     */
+    private function findAttrsByGroupId($groupId)
+    {
+        $output = array();
+
+        foreach ($this->rows as $row) {
+            if ($row[self::ARRAY_KEY_GROUP_ID] == $groupId) {
+                $output[$row[self::ARRAY_KEY_VALUE_ID]] = $row[self::ARRAY_KEY_VALUE_NAME];
+            }
+        }
+
+        return $output;
+    }
+}
