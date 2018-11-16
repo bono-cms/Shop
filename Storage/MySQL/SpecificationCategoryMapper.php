b79bc67683cf953f899683d03b7e12da9aff2147
dave-yang <daworld.ny@gmail.com>
2018-11-16 18:36:35 +0500

    Added fetchAll() and fetchById()

--- Storage/MySQL/SpecificationCategoryMapper.php
+++ Storage/MySQL/SpecificationCategoryMapper.php
@@ -31,4 +31,46 @@ final class SpecificationCategoryMapper extends AbstractMapper implements Specif
     {
         return SpecificationCategoryTranslationMapper::getTableName();
     }
+
+    /**
+     * Returns shared columns to be selected
+     * 
+     * @return array
+     */
+    private function getColumns()
+    {
+        return array(
+            self::column('id'),
+            self::column('order'),
+            SpecificationCategoryTranslationMapper::column('lang_id'),
+            SpecificationCategoryTranslationMapper::column('name')
+        );
+    }
+
+    /**
+     * Fetch all categories
+     * 
+     * @return array
+     */
+    public function fetchAll()
+    {
+        $db = $this->createEntitySelect($this->getColumns())
+                   ->whereEquals(SpecificationCategoryTranslationMapper::column('lang_id'), $this->getLangId())
+                   ->orderBy(self::column('id'))
+                   ->desc();
+
+        return $db->queryAll();
+    }
+
+    /**
+     * Fetches category by its ID
+     * 
+     * @param int $id Category id
+     * @param boolean $withTranslations Whether to fetch translations or not
+     * @return array
+     */
+    public function fetchById($id, $withTranslations)
+    {
+        return $this->findEntity($this->getColumns(), $id, $withTranslations);
+    }
 }
