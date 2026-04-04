bc59dc445fc66ab2d022de1f1c4157d86bbe825d
dave-yang <daworld.ny@gmail.com>
2026-04-04 18:09:04 +0500

    Fixed namespace issue

--- Assets/admin/product.form.js
+++ Assets/admin/product.form.js
@@ -1,86 +1,91 @@
-
 $(function(){
+    // Use the specific form as the root context
+    var $form = $('.img-upload-form');
+    
     /**
-	 * Creates row in the image table
-	 * 
-	 * @param string img blob image data
-	 * @return void
-	 */
-	function createRow(img){
-		// Temporary solution
-		var content = 
-		'<tr>' + 
-			'<td><img data-image="preview" src="' + img + '"'  + '></td>' + 
-			'<td></td>' + 
-			'<td></td>' +
-			'<td></td>' + 
-			'<td>' +
-				'<a data-button="delete" href="#"><i class="glyphicon glyphicon-remove"></i></a>' +
-			'</td>' +
-		'</tr>';
+     * Creates row in the image table
+     * 
+     * @param string img blob image data
+     * @return void
+     */
+    function createRow(img){
+        // Temporary solution
+        return `
+            <tr>
+                <td><img data-image="preview" src="${img}"></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td>
+                    <a data-button="delete" href="#">
+                        <i class="bi bi-x-lg"></i>
+                    </a>
+                </td>
+            </tr>`;
+    }
 
-		return content;
-	}
+    /**
+     * Create a new file input element
+     * @return DOMElement
+     */
+    function createFileElement(){
+        var input = document.createElement('input');
 
-	/**
-	 * Create a new file input element
-	 * 
-	 * @return DOMElement
-	 */
-	function createFileElement(){
-		var input = document.createElement('input');
+        $(input).attr({
+            type : "file",
+            name : "file[]",
+            accept : 'image/x-png, image/gif, image/jpeg'
+        });
 
-		$(input).attr({
-			type : "file",
-			name : "file[]",
-			accept : 'image/x-png, image/gif, image/jpeg'
-		});
+        // Attach the click listener now to the created file element
+        $(input).click(function(){
+            $(this).preview(function(data){
+                // Scope the container search to this form
+                $form.find("[data-container='image']").append(createRow(data));
+            });
+        });
 
-		// Attach the click listener now to the created file element
-		$(input).click(function(){
-			$(this).preview(function(data){
-				$("[data-container='image']").append(createRow(data));
-			});
-		});
-
-		return input;
-	}
-	
-	$('[data-button="upload"]').click(function(event){
-		event.preventDefault();
-		
-		var input = createFileElement();
-		
-		// Now append prepared DOM element into our container
-		$("#file-input-container").append(input);
-		
-		input.click();
-	});
-	
-	$("[data-button='edit']").click(function(event){
-		event.preventDefault();
-		
-		var id = $(this).data('image');
-		var $file = $(this).parent().find("input[type='file']");
-		var $img = $(this).parent().parent().find("td img");
-		
-		$file.preview(function(imgData){
-			$img.attr({
-				'data-image' : 'preview',
-				'src' : imgData
-			});
-		});
-		
-		$file.click();
-	});
-	
-	$(document).on('click', "[data-button='delete']", function(event){
-		event.preventDefault();
-		var $row = $(this).parent().parent();
-		
-		// Remove last input from container
-		$("#file-input-container > input:last-child").remove();
-		
-		$row.empty();
-	});
+        return input;
+    }
+    
+    // Scope the upload button listener
+    $form.find('[data-button="upload"]').click(function(event){
+        event.preventDefault();
+        
+        var input = createFileElement();
+        
+        // Scope the file input container search to this form
+        $form.find("#file-input-container").append(input);
+        
+        input.click();
+    });
+    
+    // Scope the edit button listener
+    $form.find("[data-button='edit']").click(function(event){
+        event.preventDefault();
+        
+        var id = $(this).data('image');
+        var $file = $(this).parent().find("input[type='file']");
+        var $img = $(this).parent().parent().find("td img");
+        
+        $file.preview(function(imgData){
+            $img.attr({
+                'data-image' : 'preview',
+                'src' : imgData
+            });
+        });
+        
+        $file.click();
+    });
+    
+    // Scope the delete logic to the form container
+    $form.on('click', "[data-button='delete']", function(event){
+        event.preventDefault();
+        var $row = $(this).parent().parent();
+        
+        // Scope the removal to the input container within this form
+        $form.find("#file-input-container > input:last-child").remove();
+        
+        $row.empty();
+    });
 });
